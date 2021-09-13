8f60023f60f5af7e911441ad755a93afa3feb44a
shadrack.adwera <adweshshaddie@gmail.com>
2021-09-13 12:47:00 +0300

    fix CORS error on the tasks api

--- tasks-api/tasks-app.js
+++ tasks-api/tasks-app.js
@@ -11,6 +11,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods','OPTIONS, PUT, PATCH, POST, DELETE, GET');
+  next();
+});
+
 const extractAndVerifyToken = async (headers) => {
   if (!headers.authorization) {
     throw new Error('No token provided.');
